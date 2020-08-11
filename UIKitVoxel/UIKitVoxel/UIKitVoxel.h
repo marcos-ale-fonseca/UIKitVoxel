cc7c4677c200811bccf734b3a7311f229be7e2f8
Marcos Alessndro da Fonseca <marcos.ale.fonseca@gmail.com>
2020-08-11 18:43:26 -0300

    Created project, the first commit.

---
+++ UIKitVoxel/UIKitVoxel/UIKitVoxel.h
@@ -0,0 +1,19 @@
+//
+//  UIKitVoxel.h
+//  UIKitVoxel
+//
+//  Created by MARCOS ALESSANDRO DA FONSECA on 11/08/20.
+//  Copyright © 2020 MARCOS ALESSANDRO DA FONSECA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIKitVoxel.
+FOUNDATION_EXPORT double UIKitVoxelVersionNumber;
+
+//! Project version string for UIKitVoxel.
+FOUNDATION_EXPORT const unsigned char UIKitVoxelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitVoxel/PublicHeader.h>
+
+
---
+++ UIKitVoxelExample/UIKitVoxelExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitVoxelExample
+//
+//  Created by MARCOS ALESSANDRO DA FONSECA on 11/08/20.
+//  Copyright © 2020 MARCOS ALESSANDRO DA FONSECA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
